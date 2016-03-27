7246f2a7dfe3f85719e9a3d61e44239fcba2737a
David Parry <david.parry@suranyami.com>
2016-03-27 16:24:02 +1100

    removed useless comments

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -1,12 +1,9 @@
 defmodule Mugofccino.UserSocket do
   use Phoenix.Socket
 
-  ## Channels
   channel "rooms:*", Mugofccino.RoomChannel
 
-  ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
