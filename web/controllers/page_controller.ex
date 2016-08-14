0a74d8074f8b3bf9cfdc7d21a5ebd45468e5c905
David Parry <david.parry@suranyami.com>
2016-08-15 06:20:46 +1000

    playing with pattern-matching in controller actions

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule Mugofccino.PageController do
   use Mugofccino.Web, :controller
 
+  def index(conn, params = %{name: name}) do
+    render conn, "index.html", name: name
+  end
+
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", name: "Phoenix!"
   end
 end
