95c78514584d43a029483022638922866bf08890
David Parry <david.parry@suranyami.com>
2016-08-26 16:24:33 +1000

    load react

--- brunch-config.js
+++ brunch-config.js
@@ -52,6 +52,7 @@ exports.config = {
   // Configure your plugins
   plugins: {
     babel: {
+      presets: ["es2015", "react"],
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
     }
@@ -64,6 +65,9 @@ exports.config = {
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    // Whitelist the npm deps to be pulled in as front-end assets.
+    // All other deps in package.json will be excluded from the bundle.
+    whitelist: ["phoenix", "phoenix_html", "react", "react-dom"]
   }
 };
