2988ce3913bebcd6b5fbf7b7f4260ae82eae9c79
David Parry <david.parry@suranyami.com>
2016-09-05 09:38:59 +1000

    disable sockets for now

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,7 +1,9 @@
-import "phoenix_html"
-import socket from "./socket"
+import 'phoenix_html'
+// import socket from './socket'
 
-import {Game} from "./Game"
+import {Game} from './Game'
 
 const assetPath = document.body.dataset.assetPath
-const game = new Game(700, 450, "screen", assetPath)
+const game = new Game(700, 450, 'screen', assetPath)
+
+game.start()
