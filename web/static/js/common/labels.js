6104e1fb3c5cb3fb8b16204402d88523895b5c5b
David Parry <david.parry@suranyami.com>
2016-09-05 09:38:39 +1000

    using carbon font

--- web/static/js/common/labels.js
+++ web/static/js/common/labels.js
@@ -1,10 +1,15 @@
 const DEFAULT_STYLE = {
-  font: "65px Helvetica",
-  fill: "#ff0000"
+  font: '65px Carbon',
+  fill: 'green',
+  align: 'center'
 }
 
 // createLabel :: State -> String -> Object -> Sprite
 export const createLabel = function (state, message, style = DEFAULT_STYLE) {
   const {centerX, centerY} = state.world
-  return state.add.text(centerX, centerY, message, style)
+  let label = state.add.text(centerX, centerY, message, style)
+  label.stroke = '#ffffff'
+  label.strokeThickness = 2
+  label.setShadow(4, 4, '#333333', 4, true, false)
+  return label
 }
