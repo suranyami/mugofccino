94ec19b46f9074478d2225f97c0c67ff26344274
David Parry <david.parry@suranyami.com>
2016-09-06 23:30:37 +1000

    passing in socket to game

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,9 +1,8 @@
 import 'phoenix_html'
-// import socket from './socket'
-
+import socket from './socket'
 import {Game} from './Game'
 
 const assetPath = document.body.dataset.assetPath
-const game = new Game(700, 450, 'screen', assetPath)
+const game = new Game(800, 600, 'screen', assetPath)
 
-game.start()
+game.start(socket)
