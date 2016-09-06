e8fde2d4b06b699c9dc36bbe7ffd9d8819596463
David Parry <david.parry@suranyami.com>
2016-09-06 23:31:32 +1000

    setup player

--- web/static/js/Player.js
+++ web/static/js/Player.js
@@ -1,10 +1,15 @@
-class Player extends Phaser.State {
-  init(options) {
-    game = options.game
-    this.sprite = game.add.sprite(midWidth, midHeight, 'player')
-    this.sprite.anchor.setTo(0.5, 0.5)
-    this.game.physics.enable(sprite, Phaser.Physics.ARCADE)
-    this.sprite.body.maxVelocity.setTo(MAXSPEED, MAXSPEED)
-    this.sprite.body.drag.setTo(DRAG, DRAG)
+const MAXSPEED = 100
+const DRAG = 50
+
+export class Player extends window.Phaser.Sprite {
+  constructor (game) {
+    super(game, game.world.centerX, game.world.centerY, 'player')
+    this.anchor.setTo(0.5, 0.5)
+    game.physics.enable(this, window.Phaser.Physics.ARCADE)
+  }
+
+  create () {
+    this.body.maxVelocity.setTo(MAXSPEED, MAXSPEED)
+    this.body.drag.setTo(DRAG, DRAG)
   }
 }
