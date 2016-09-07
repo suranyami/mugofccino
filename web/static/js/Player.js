280d306df83048e9986ec112d6857f5047cfd8a4
David Parry <david.parry@suranyami.com>
2016-09-07 16:59:29 +1000

    don't extend Phaser.Sprite... weird stuff happening
    Just use composition.

--- web/static/js/Player.js
+++ web/static/js/Player.js
@@ -1,42 +1,64 @@
-const MAXSPEED = 100
-const DRAG = 50
+const MAXSPEED = 400
+const DRAG = 400
+const BULLET_SPEED = 400
+const BULLET_FIRE_RATE = 250
 
-export class Player extends window.Phaser.Sprite {
+// const ACCELERATION = 600
+
+export class Player {
   constructor (game) {
-    super(game, game.world.centerX, game.world.centerY, 'player')
-    this.anchor.setTo(0.5, 0.5)
-    game.physics.enable(this, window.Phaser.Physics.ARCADE)
-    this.cursors = this.game.input.keyboard.createCursorKeys()
-    this.game.input.keyboard.addKeyCapture([window.Phaser.Keyboard.SPACEBAR])
+    console.log('Player.constructor')
+    this.game = game
   }
 
-  create () {
-    this.body.maxVelocity.setTo(MAXSPEED, MAXSPEED)
-    this.body.drag.setTo(DRAG, DRAG)
+  start () {
+    this.sprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player')
+    this.sprite.anchor.setTo(0.5, 0.5)
+    this.game.physics.enable(this.sprite, window.Phaser.Physics.ARCADE)
+    this.cursors = this.game.input.keyboard.createCursorKeys()
+    this.game.input.keyboard.addKeyCapture([window.Phaser.Keyboard.SPACEBAR])
+    this.sprite.body.maxVelocity.setTo(MAXSPEED, MAXSPEED)
+    this.sprite.body.drag.setTo(DRAG, DRAG)
+    this.makeBullets()
+  }
+
+  makeBullets () {
+    console.log('Player.makeBullets')
+    this.bullets = this.game.add.weapon(30, 'bullet')
+    this.bullets.bulletKillType = window.Phaser.Weapon.KILL_WORLD_BOUNDS
+    this.bullets.bulletSpeed = BULLET_SPEED
+    this.bullets.fireRate = BULLET_FIRE_RATE
+
+    this.explosion = this.game.add.audio('explosion')
+    this.blaster = this.game.add.audio('blaster')
+    // this.game.sound.setDecodedCallback([this.explosion, this.blaster], this.start, this)
+    let pew = () => {
+      this.blaster.play()
+    }
+    this.bullets.onFire.add(pew)
+    this.game.physics.enable(this.bullets, window.Phaser.Physics.ARCADE)
+    this.bullets.trackSprite(this.sprite, 16, 0, true)
   }
 
   update () {
     if (this.cursors.up.isDown) {
-      this.game.physics.arcade.accelerationFromRotation(this.rotation, 300, this.body.acceleration)
+      this.game.physics.arcade.accelerationFromRotation(this.sprite.rotation, 300, this.sprite.body.acceleration)
     } else {
-      this.body.acceleration.set(0)
+      this.sprite.body.acceleration.set(0)
     }
 
     if (this.cursors.left.isDown) {
-      this.body.angularVelocity = -300
+      this.sprite.body.angularVelocity = -300
     } else if (this.cursors.right.isDown) {
-      this.body.angularVelocity = 300
+      this.sprite.body.angularVelocity = 300
     } else {
-      this.body.angularVelocity = 0
+      this.sprite.body.angularVelocity = 0
     }
 
     if (this.game.input.keyboard.isDown(window.Phaser.Keyboard.SPACEBAR)) {
-      console.log('pew pew')
-      // bullets.fire()
+      this.bullets.fire()
     }
 
-    this.game.world.wrap(this, 16, true)
-    // this.game.physics.arcade.overlap(bullets.bullets, aliens, collisionHandler, null, this);
-    // this.game.physics.arcade.overlap(player, aliens, collisionHandler, null, this);
+    this.game.world.wrap(this.sprite, 16, true)
   }
 }
