9e9ebc50db6946eec3111472d0f120a26baffce6
David Parry <david.parry@suranyami.com>
2016-09-07 12:03:40 +1000

    DRYed up asset loading, added bullets, explosion, sounds

--- web/static/js/states/Boot.js
+++ web/static/js/states/Boot.js
@@ -1,13 +1,46 @@
+// Preloads the assets
 export class Boot extends window.Phaser.State {
   preload () {
     console.log('Boot.preload')
-    let load = this.game.load
-    load.image('player', '/images/player.png')
-    load.image('starfield', '/images/starfield.png')
-    load.image('enemy', '/images/enemy-blue.png')
+    this.assetPath = document.body.dataset.assetPath
+    this.imagePath = `${this.assetPath}images`
+    this.soundPath = `${this.assetPath}sounds`
+    this.loadImage('player')
+    this.loadImage('starfield')
+    this.loadImage('enemy', 'enemy-blue')
+    this.loadImage('bullet')
+    this.spriteFor('explosion', 128, 128, 'explode')
+    this.loadSound('explosion')
+    this.loadSound('blaster')
   }
 
   create () {
     this.game.state.start('lobby', true, false)
+  }
+
+  spriteFor (name, width, height, filename = name, suffix = 'png') {
+    let path = this.imageFor(filename, suffix)
+    this.game.load.spritesheet(name, path, width, height)
+    console.log(`sprite: '${name}', '${path}', ${width}, ${height}`)
+  }
+
+  soundFor (name, suffix = 'mp3') {
+    return `${this.soundPath}/${name}.${suffix}`
+  }
+
+  loadSound (name, filename = name) {
+    let path = this.soundFor(filename)
+    this.game.load.audio(name, path)
+    console.log(`sound: '${name}', '${path}'`)
+  }
+
+  imageFor (name, suffix = 'png') {
+    return `${this.imagePath}/${name}.${suffix}`
+  }
+
+  loadImage (name, filename = name) {
+    let path = this.imageFor(filename)
+    this.game.load.image(name, path)
+    console.log(`image: '${name}', '${path}'`)
   }
 }
