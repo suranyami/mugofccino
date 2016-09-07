2a2cef7e92fc2c3c3bc805f6cd28d782bad2b010
David Parry <david.parry@suranyami.com>
2016-09-07 16:57:42 +1000

    player & star field being added to game, not as addChild
    It seemed to be the cause of the problem with bullets not appearing.

--- web/static/js/states/MainScreen.js
+++ web/static/js/states/MainScreen.js
@@ -2,15 +2,24 @@ import {Player} from '../Player'
 
 export class MainScreen extends window.Phaser.State {
   create () {
-    console.log('creating main_screen')
-    this.physics.startSystem(window.Phaser.Physics.P2JS)
+    console.log('MainScreen.create')
+    this.game.renderer.clearBeforeRender = false
+    this.game.renderer.roundPixels = true
+    this.game.add.tileSprite(0, 0, 800, 600, 'starfield')
+    this.game.physics.startSystem(window.Phaser.Physics.ARCADE)
     this.player = new Player(this.game)
-    this.starfield = this.game.add.tileSprite(0, 0, 800, 600, 'starfield')
-    this.game.stage.addChild(this.starfield)
-    this.game.stage.addChild(this.player)
+    this.player.start()
   }
 
   update () {
+    this.player.update()
+    // this.game.physics.arcade.overlap(this.game.bullets.this.game.bullets, aliens, collisionHandler, null, this)
+    // this.game.physics.arcade.overlap(player, aliens, collisionHandler, null, this)
+  }
+
+  render () {
+    // this.game.debug.text('help', 32, 32)
+    // this.player.bullets.debug()
   }
 
   shutdown () {
