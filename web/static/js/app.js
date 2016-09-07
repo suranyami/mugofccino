abcddb457c3afaf83776550f0e1a63ddac8c7d28
David Parry <david.parry@suranyami.com>
2016-09-07 16:17:35 +1000

    simplified game creation... removing things that can go wrong

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,8 +1,23 @@
 import 'phoenix_html'
-import socket from './socket'
-import {Game} from './Game'
+// import socket from './socket'
+import {Boot} from './states/Boot'
+import {Lobby} from './states/Lobby'
+import {MainScreen} from './states/MainScreen'
 
-const assetPath = document.body.dataset.assetPath
-const game = new Game(800, 600, 'screen', assetPath)
+let game = new window.Phaser.Game(800, 600, window.Phaser.AUTO, 'screen')
+game.assetPath = document.body.dataset.assetPath
+game.state.add('boot', Boot, false)
+game.state.add('lobby', Lobby, false)
+game.state.add('main_screen', MainScreen, false)
+game.backgroundColor = '#000000'
 
-game.start(socket)
+console.log('Game.start')
+game.state.start('boot', true, false)
+
+    // socket.connect()
+    // const channel = socket.channel('games:lobby', {})
+
+    // joinChannel(channel, () => {
+    //   console.log('Joined successfully')
+    //   this.state.start('lobby', true, false, channel)
+    // })
