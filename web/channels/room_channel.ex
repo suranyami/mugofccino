e8a0f927d049ad23597600af4fc28f8c7b830d49
David Parry <david.parry@suranyami.com>
2016-09-08 20:23:37 +1000

    added a unique id to each player, assigned to socket

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,17 +1,29 @@
 defmodule Mugofccino.RoomChannel do
   use Phoenix.Channel
 
-  def join("topic:" <> _topic_name, _auth_msg, socket) do
+  def join("games:" <> _topic, _auth_message, socket) do
+    send(self, :after_join)
     {:ok, socket}
   end
 
-  def handle_in("message", %{"body" => body}, socket) do
-    broadcast! socket, "message", %{body: body}
+  def handle_info(:after_join, socket) do
+    # uuid = UUID.uuid1()
+    uuid = :rand.uniform(1000)
+    socket = assign(socket, :uuid, uuid)
+    id = %{uuid: uuid}
+    push socket, "joined", id
+    broadcast! socket, "opponent_joined", id
     {:noreply, socket}
   end
 
-  # def handle_out("new_msg", payload, socket) do
-  #   push socket, "new_msg", payload
+  def handle_in("move", message, socket) do
+    uuid = socket.assigns[:uuid]
+    broadcast! socket, "opponent_move", Map.put(message, :uuid, uuid)
+    {:noreply, socket}
+  end
+
+  # def handle_out("message", message, socket) do
+  #   push socket, "message", message
   #   {:noreply, socket}
   # end
 
